d73c08ab230d77bf47008e3ca2c55f0457847f88
byeweb2 <sy47525@gmail.com>
2022-09-07 23:05:30 +0100

    Add todo list task and chnage some style

---
+++ src/js/main.js
@@ -0,0 +1,28 @@
+// Selectors
+document.querySelector('form').addEventListener('submit', handleSubmitForm);
+
+// Event handlers
+function handleSubmitForm(e) {
+    e.preventDefault();
+    let input = document.querySelector('input');
+    if (input.value != '') {
+        addTodo(input.value);
+    }
+    input.value = '';
+}
+
+// Helpers 
+function addTodo(todo) {
+    let ul = document.querySelector('ul');
+    let li = document.createElement('li');
+    
+    li.innerHTML = `
+        <span class="todo-item">${todo}</span>
+        <button class="checkBtn"><i class="fas fa-check-square"></i></button>
+        <button class="deleteBtn"><i class="fas fa-trash"></i></button>
+    `;
+
+    li.classList.add('todo-list-item');
+    ul.appendChild(li);
+
+}
